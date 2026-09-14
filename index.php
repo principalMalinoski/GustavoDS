5555c0c16af26970830ee33de10be3e95af80703
principalMalinoski <gmalinoskiamorim@gmail.com>
2026-09-14 16:27:23 -0300

    alteração maior ou menor de idade

--- index.php
+++ index.php
@@ -1,23 +1,31 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AULAS DS</title>
 </head>
 <body>
+
 <?php
 
-
 $nome = "Gustavo";
-$idade = "16";
+$idade = 16;
 
- ?>
+?>
 
- <h1>nome: <?= $nome ?></h1>
- <p>idade: <?= $idade ?></p>
+<h1>Nome: <?= $nome ?></h1>
+<p>Idade: <?= $idade ?></p>
 
+<?php
+
+if ($idade >= 18) {
+    echo "<p>Gustavo é maior de idade.</p>";
+} else {
+    echo "<p>Gustavo é menor de idade.</p>";
+}
+
+?>
 
 </body>
 </html>
