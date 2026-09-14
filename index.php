a761f6c431ffb6bde767af7af3cb3ec664d8d81e
principalMalinoski <gmalinoskiamorim@gmail.com>
2026-09-14 16:53:13 -0300

    versao certa

--- index.php
+++ index.php
@@ -6,26 +6,22 @@
     <title>AULAS DS</title>
 </head>
 <body>
+<?php 
 
-<?php
+$nome = "Gustavo"; 
+$idade = 16; 
 
-$nome = "Gustavo";
-$idade = 16;
-
+if ($idade >= 18) { 
+    $status = "Você é maior de idade"; 
+} else { 
+    $status = "Você é menor de idade"; 
+} 
 ?>
 
-<h1>Nome: <?= $nome ?></h1>
-<p>Idade: <?= $idade ?></p>
-<p>status <?= $status ?></p>
-<?php
+<h1>Nome: <?= $nome ?></h1> 
+<p>Idade: <?= $idade ?></p> 
+<p>Status: <?= $status ?></p>
 
-if ($idade >= 18) {
-    $status = "Você é maior de idade";
-} else {
-    $status = "Você é menor de idade";
-}
-
-?>
 
 </body>
 </html>
