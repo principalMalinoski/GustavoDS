471e64eb292688138c863eddae09228076469584
principalMalinoski <gmalinoskiamorim@gmail.com>
2026-09-14 15:30:48 -0300

    Update index.php
    mudança php

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DAu</title>
+</head>
+<body>
 <?php
-echo "<h1>Aula DS do SENAI</h1>";
+
+ echo "<h1>Aula DS do SENAI</h1>";
+
+ ?>
+</body>
+</html>
