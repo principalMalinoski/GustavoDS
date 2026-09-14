6d68451d8bb782130be3f280dccd452b991a91a0
principalMalinoski <gmalinoskiamorim@gmail.com>
2026-09-14 15:31:45 -0300

    aulas ds

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DAu</title>
+    <title>AULAS DS</title>
 </head>
 <body>
 <?php
