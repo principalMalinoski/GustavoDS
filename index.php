2dd8950ec688fdb9e9fdf593d86fb92d19dd783b
principalMalinoski <gmalinoskiamorim@gmail.com>
2026-09-15 17:13:30 -0300

    Update index.php

--- index.php
+++ index.php
@@ -24,10 +24,8 @@ $resultado="";
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>AULAS DS</title>
-        
-        <link rel="stylesheet" href="style.css">
-    </head>
-
+        <link rel="stylesheet" href="css/style.css">
+    
     </head>
     <body>
 
