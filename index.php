6f56940ae387bdf780fa3a63af395fb7eb67eea6
principalMalinoski <gmalinoskiamorim@gmail.com>
2026-09-15 16:56:11 -0300

    mudanças do css e o index

--- index.php
+++ index.php
@@ -24,10 +24,9 @@ $resultado="";
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>AULAS DS</title>
-        <meta charset="UTF-8">
-        <title>Meu Site</title>
+        
         <link rel="stylesheet" href="style.css">
-</head>
+    </head>
 
     </head>
     <body>
