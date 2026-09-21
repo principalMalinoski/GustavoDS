b5da9687e6d1e515a71c53f9f15b0873d2d5d1db
principalMalinoski <gmalinoskiamorim@gmail.com>
2026-09-21 16:29:45 -0300

    Update notas.php

--- notas.php
+++ notas.php
@@ -4,7 +4,7 @@ $idade = 0;
 $media = 0;
 $situação = "";
 
-POST if ($_SERVER["REQUEST_METHOD"] === "POST") {
+ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 $nome = trim($_POST["nome"] ?? "");
 $idade = (int) ($_POST["idade"] ?? 0);
@@ -12,7 +12,7 @@ $nota1 = (float) ($_POST["nota1"] ?? 0);
 $nota2 = (float) ($_POST["nota2"] ?? 0);
 $nota3 = (float) ($_POST["nota3"] ?? 0);
 $nota4 = (float) ($_POST["nota4"] ?? 0);
-$nota5 = (float) ($_POST["nota5"] ?? 0):
+$nota5 = (float) ($_POST["nota5"] ?? 0);
 
 $media = ($nota1 * 2 + $nota2 * 3 + $nota3 * 1 + $nota4 * 1 + $nota5 * 3) / 10;
 
