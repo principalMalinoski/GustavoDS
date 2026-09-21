c8275d70230ec4ebe3cea6966641eae177d44430
principalMalinoski <gmalinoskiamorim@gmail.com>
2026-09-21 16:09:01 -0300

    notas e int

--- notas.php
+++ notas.php
@@ -3,3 +3,17 @@ $nome = "";
 $idade = 0;
 $media = 0;
 $situação = "";
+
+POST if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+$nome = trim($_POST["nome"] ?? "");
+$idade = (int) ($_POST["idade"] ?? 0);
+$nota1 = (float) ($_POST["nota1"] ?? 0);
+$nota2 = (float) ($_POST["nota2"] ?? 0);
+$nota3 = (float) ($_POST["nota3"] ?? 0);
+
+
+
+
+
+}
