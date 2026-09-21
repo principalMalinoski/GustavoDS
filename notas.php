46f8a085c13c04ea8885d178f45094595559508b
principalMalinoski <gmalinoskiamorim@gmail.com>
2026-09-21 16:15:31 -0300

    Update notas.php

--- notas.php
+++ notas.php
@@ -12,7 +12,9 @@ $nota1 = (float) ($_POST["nota1"] ?? 0);
 $nota2 = (float) ($_POST["nota2"] ?? 0);
 $nota3 = (float) ($_POST["nota3"] ?? 0);
 $nota4 = (float) ($_POST["nota4"] ?? 0);
+$nota5 = (float) ($_POST["nota5"] ?? 0):
 
+$media = ($nota1 * 2 + $nota2 * 3 + $nota3 * 1 + 4nota4 * 1 + $nota5 * 3) / 10;
 
 
 
